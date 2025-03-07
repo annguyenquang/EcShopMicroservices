4ba1e0fd3e218d0254d50acbde810c706d071c44
aGoodMan999 <21521808@gm.uit.edu.vn>
2025-03-07 18:20:16 +0700

    update: add update product validator

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Catalog.API.Products.UpdateProduct;
+﻿namespace Catalog.API.Products.UpdateProduct;
 
 public record UpdateProductCommand(
     Guid Id,
@@ -11,6 +10,28 @@ public record UpdateProductCommand(
     ) : ICommand<UpdateProductCommandResult>;
 
 public record UpdateProductCommandResult(bool IsSuccess);
+
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    private const int MAX_NAME_LENGTH = 150;
+    private const int MIN_NAME_LENGTH = 2;
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+                .WithMessage("Product Id is required");
+        
+        RuleFor(x => x.Name)
+            .NotEmpty()
+                .WithMessage("Name is required")
+            .Length(MIN_NAME_LENGTH, MAX_NAME_LENGTH)
+                .WithMessage($"Name must be between {MIN_NAME_LENGTH} and {MAX_NAME_LENGTH} characters");
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0)
+                .WithMessage("Price must be greater than 0");
+    }
+}
 
 internal class UpdateProductHandler(IDocumentSession session, ILogger<UpdateProductHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductCommandResult>
 {
